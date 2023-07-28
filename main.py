86aaa3cf70e3851691c8bb15e056873323c6fde2
AlexPakhomchik <korsar7772@gmail.com>
2023-07-28 11:39:24 +0300

    connect to db

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from database.connection import conn
+from routes.events import event_router
 from routes.todo import todo_router
 from routes.users import user_router
 
@@ -14,6 +16,11 @@ async def welcome() -> dict:
 
 app.include_router(todo_router)
 app.include_router(user_router, prefix='/user')
+app.include_router(event_router, prefix='/event')
+
+@app.on_event('startup')
+def on_startup():
+    conn()
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8080, reload=True)
